92d6f4dd2290d42f4156717c40249ae82d15e93c
Mayra Jaimes <mtj26@georgetown.edu>
2017-12-23 14:35:23 -0600

    homework I am turning in

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,123 +10,114 @@ $(document).ready(function() {
     var rightanswer;
     var rightImage;
     var intervalId;
+    var getStartButton = document.getElementById("startButton");
+    var getStartPage = document.getElementById("startPage");
     var getTriviaPage = document.getElementById("triviaPage");
+    var getTimer = document.getElementById("timer");
     var getTimeOutPage = document.getElementById("timeOutPage");
     var getRightAnswerPage = document.getElementById("rightAnswerPage");
-    var getEndPage = document.getElementById("endPage");
     var getWrongAnswerPage = document.getElementById("wrongAnswerPage");
-    var getStartPage = document.getElementById("startPage");
-    var getTimer = document.getElementById("timer");
+    var getEndPage = document.getElementById("endPage");
     var getResetButton = document.getElementById("resetButton");
-    var getStartButton = document.getElementById("startButton");
-
     var randomizeArray = arr => arr.sort(() => Math.random() - 0.5);
-
     var trivia = [{
-            question: "In Disney's 'The Little Mermaid' who does Ariel fall in love with?",
-            choices: ["Prince Eric",
-                "Prince Phillip",
-                "Prince Charming",
-                "Prince Joseph"],
-            answer: "Prince Eric",
-            image: "image0.jpg"
+        question: "In Disney's 'The Little Mermaid' who does Ariel fall in love with?",
+        choices: ["Prince Eric",
+            "Prince Phillip",
+            "Prince Charming",
+            "Prince Joseph"],
+        answer: "Prince Eric",
+        image: "image0.jpg"
         },
 
-        {
-            question: "What was the name of the monkey in the Disney movie 'Aladdin'?",
-            choices: ["Abu",
-                "Donkey Kong",
-                "Rafiki",
-                "Jack"],
-            answer: "Abu",
-            image: "image1.jpg"
+        {question: "What was the name of the monkey in the Disney movie 'Aladdin'?",
+        choices: ["Abu",
+            "Donkey Kong",
+            "Rafiki",
+            "Jack"],
+        answer: "Abu",
+        image: "image1.jpg"
         },
 
-        {
-            question: "What is the name of Mickey Mouse´s dog?",
-            choices: ["Pluto",
-                "Donald",
-                "Bolt",
-                "Oliver"],
-            answer: "Pluto",
-            image: "image2.jpg"
+        {question: "What is the name of Mickey Mouse´s dog?",
+        choices: ["Pluto",
+            "Donald",
+            "Bolt",
+            "Oliver"],
+        answer: "Pluto",
+        image: "image2.jpg"
         },
 
-        {
-            question: "What is the name of one of the 7 dwarfs from the Disney movie 'Snow White and the Seven Dwarfs'?",
-            choices: ["Moody",
-                "Joyful",
-                "Lazy",
-                "Happy"],
-            answer: "Happy",
-            image: "image3.jpg"
+        {question: "What is the name of one of the 7 dwarfs from the Disney movie 'Snow White and the Seven Dwarfs'?",
+        choices: ["Moody",
+            "Joyful",
+            "Lazy",
+            "Happy"],
+        answer: "Happy",
+        image: "image3.jpg"
         },
 
-        {
-            question: "In which Disney film will you find the villain, Cruella De Vil?",
-            choices: ["Dalmatians",
-                "Cinderella",
-                "Sleeping Beauty",
-                "Lion King"],
-            answer: "Dalmatians",
-            image: "image4.jpg"
+        {question: "In which Disney film will you find the villain, Cruella De Vil?",
+        choices: ["Dalmatians",
+            "Cinderella",
+            "Sleeping Beauty",
+            "Lion King"],
+        answer: "Dalmatians",
+        image: "image4.jpg"
         },
 
-        {
-            question: "What animals portray surfer dudes in Finding Nemo?",
-            choices: ["Turtles",
-                "Sharks",
-                "Seals",
-                "Dolphins"],
-            answer: "Turtles",
-            image: "image5.jpg"
+        {question: "What animals portray surfer dudes in Finding Nemo?",
+        choices: ["Turtles",
+            "Sharks",
+            "Seals",
+            "Dolphins"],
+        answer: "Turtles",
+        image: "image5.jpg"
         },
 
-        {
-            question: "What is the name of Woody's owner in Toy Story?",
-            choices: ["Andy",
-                "Ryan",
-                "Sid",
-                "Lenny"],
-            answer: "Andy",
-            image: "image6.jpg"
+        {question: "What is the name of Woody's owner in Toy Story?",
+        choices: ["Andy",
+            "Ryan",
+            "Sid",
+            "Lenny"],
+        answer: "Andy",
+        image: "image6.jpg"
         },
 
-        {
-            question: "What puts Snow White into a deep sleep?",
-            choices: ["A poisoned apple",
-                "A needle",
-                "A car accident",
-                "A lost slipper"
-            ],
-            answer: "A poisoned apple",
-            image: "image7.jpg"
+        {question: "What puts Snow White into a deep sleep?",
+        choices: ["A poisoned apple",
+            "A needle",
+            "A car accident",
+            "A lost slipper"
+        ],
+        answer: "A poisoned apple",
+        image: "image7.jpg"
         }
     ]
 
     for (i = 0; i < trivia.length; i++) {
         randomChoices = randomizeArray(trivia[i].choices);
         questionHTML +=
-            `<div id="question_${i}" class="questions-group">
-                    <div id="answer_${i}" class="answerSheet">
-                        ${trivia[i].answer}
-                    </div>
-                    <div id="image_${i}" class="imageSheet">
-                        ${trivia[i].image}
-                    </div>
-    				<div class="questionText">
-    				    ${trivia[i].question}
-    				</div>
-    				<div class="buttons"> 
-    					<button class="choiceButton">${trivia[i].choices[0]}</button>
-    					<button class="choiceButton">${trivia[i].choices[1]}</button>
-    					<button class="choiceButton">${trivia[i].choices[2]}</button>
-    					<button class="choiceButton">${trivia[i].choices[3]}</button>
-    				</div>
-                    <div class="questionCounter">
-                        Question ${questionCounter} out of 8
-                    </div>
-    			</div>`
+        `<div id="question_${i}" class="questions-group">
+            <div id="answer_${i}" class="answerSheet">
+                ${trivia[i].answer}
+            </div>
+            <div id="image_${i}" class="imageSheet">
+                ${trivia[i].image}
+            </div>
+			<div class="questionText">
+			    ${trivia[i].question}
+			</div>
+			<div class="buttons"> 
+				<button class="choiceButton">${trivia[i].choices[0]}</button>
+				<button class="choiceButton">${trivia[i].choices[1]}</button>
+				<button class="choiceButton">${trivia[i].choices[2]}</button>
+				<button class="choiceButton">${trivia[i].choices[3]}</button>
+			</div>
+            <div class="questionCounter">
+                Question ${questionCounter} out of 8
+            </div>
+		</div>`
     }
 
     getTriviaPage.innerHTML = questionHTML;
